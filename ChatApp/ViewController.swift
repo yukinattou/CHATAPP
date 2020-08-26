475f4c86a966fbeda89813bfdb0635158e0dece0
nonyuki <yukinattou@gmail.com>
2020-08-26 12:01:26 +0900

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by 角田幸奈 on 2020/08/26.
+//  Copyright © 2020 角田幸奈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
